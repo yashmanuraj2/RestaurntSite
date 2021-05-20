fdb4d19a09d2924a28364fb930c580f7ed346ce1
Yash Raj Singh <yashmanuraj2@gmail.com>
2021-05-20 22:40:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import DishDetail from './components/DishDetailComponent';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+const store = ConfigureStore();
+
+class App extends Component {
+  
+  render() {
+    return (
+      <Provider store={store}>
+     <BrowserRouter>
+     <div>
+
+       <Main/>
+     </div>
+     
+     </BrowserRouter>
+      </Provider>
+        
+        
+        
+      
+      
+      
+    );
+  }
+}
+
+export default App;
+
